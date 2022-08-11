6aa2c8ab2b649b4207ef41f8feb97bb6c59e90ec
Lam <lnhlam99@gmail.com>
2022-08-11 14:43:48 +0700

    fearture-01-convert-fearture

---
+++ thuc-hanh-swich-Case-JavaScript.js
@@ -0,0 +1,14 @@
+var opera = "+"
+var a = 6;
+var b = 3;
+switch (opera)
+{
+    case '+'
+        alert("a + b ="+(a+b));
+        break;
+    case '-'
+        alert("a - b ="+(a-b));
+        break;
+    case'*'
+        alert("a*b = " + (a*b));
+}
